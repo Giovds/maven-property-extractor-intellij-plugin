b37da189bcc132842a1401ff01c3cf1a88b61b1c
Giovanni van der Schelde <gvdschelde@gmail.com>
2024-10-21 20:42:15 +0200

    Support newest IntelliJ version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.giovds"
-version = "1.0.3"
+version = "1.0.4"
 
 repositories {
     mavenCentral()
@@ -36,7 +36,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("222")
-        untilBuild.set("241.*")
+        untilBuild.set("242.*")
     }
 
     signPlugin {
